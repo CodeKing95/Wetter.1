d92d4d7f951585ee22c0bbcfb2c212cd87388836
Nadir Ahmed <itna@AMAUK.local>
2024-12-18 16:11:50 +0000

    first commit

--- styles/vite.config.js
+++ styles/vite.config.js
@@ -1,3 +1,7 @@
-export default {
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
   base: "/Wetter1/",
-};
+});
